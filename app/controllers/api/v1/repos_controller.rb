1c94bbe471b08ad7266cc8238dc59e08a77693bc
Anthony Limere <anthonylimere@gmail.com>
2018-04-16 19:17:40 +0200

    Add json rendering

--- app/controllers/api/v1/repos_controller.rb
+++ app/controllers/api/v1/repos_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::ReposController < ApplicationController
   def index
     repos = Repo.all
 
-    console.log(repos)
+    render :json => repos
   end
 
   def show
     repos = Repo.all
 
-    console.log(repos)
+    render :json => repos
   end
 end
